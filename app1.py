65255cc3850d3f12d1f740eff3957f701019083b
Hema <hemaselvam0304.july@gmail.com>
2026-04-21 07:57:02 +0530

    changes of env

--- app1.py
+++ app1.py
@@ -1,14 +1,46 @@
-import streamlit as st 
+# #AI Search Assistant using Agent + Tools (LangChain + Groq)
+# AI decides:“Do I know this?”, “Should I use a tool?”
+# Uses tools like:Web Search,Wikipedia, Arxiv Papers
+
+
+
+
+
+import streamlit as st
+ 
 from langchain_groq import ChatGroq
-from langchain_community.utilities import ArxivAPIWrapper, WikipediaAPIWrapper
+
+from langchain_community.utilities import ArxivAPIWrapper, WikipediaAPIWrapper  #Wrap external APIs into usable format
 from langchain_community.tools import (
     ArxivQueryRun,
     WikipediaQueryRun,
     DuckDuckGoSearchRun
 )
+#WikipediaQueryRun → fetch Wikipedia info
+# ArxivQueryRun → fetch research papers
+# DuckDuckGoSearchRun → web search
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+from langchain_groq import ChatGroq
+
+llm = ChatGroq(
+    groq_api_key=os.getenv("GROQ_API_KEY"),
+    model_name="llama3-8b-8192"
+)
+
+
 from langchain.agents import AgentExecutor, create_tool_calling_agent
+#Core of decision-making system
+
+
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.callbacks import StreamlitCallbackHandler
+#Shows step-by-step reasoning in UI
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -27,7 +59,7 @@ search = DuckDuckGoSearchRun(name="Search")
 tools = [search, arxiv, wiki]
 
 # ---------------- UI ---------------- #
-st.title("🔍 AI Search Assistant (Groq + LangChain)")
+st.title("AI Search Assistant (Groq + LangChain)")
 
 api_key = st.sidebar.text_input("Enter Groq API Key", type="password")
 
@@ -55,7 +87,7 @@ if prompt := st.chat_input("Ask anything..."):
     prompt_template = ChatPromptTemplate.from_messages([
         ("system", "You are a smart assistant that uses tools when needed."),
         ("human", "{input}"),
-        ("placeholder", "{agent_scratchpad}")
+        ("placeholder", "{agent_scratchpad}")  #Internal memory for agent Stores:Tool calls,Intermediate reasoning
     ])
 
     # Agent
